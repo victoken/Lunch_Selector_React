90e4c1520a0f9bc0b8b3b857b5e6b4d00bc100f7
Vcie <vcie2210@gmail.com>
2024-06-18 22:12:35 +0800

    update:change setting

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
